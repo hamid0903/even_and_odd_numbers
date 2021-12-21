6b628fda12f7a7d0487b8bf8ee86b18a822677c7
Hamid Kamalov <hamid_kamalov@mail.ru>
2021-12-21 22:00:19 +0500

    problem6

--- sum_odd.py
+++ sum_odd.py
@@ -5,3 +5,16 @@
 #Create a variable "sum_even" and assign it 0.
 
 #Find the sum of the odd digits in the variable "var_int".
+var_int=7061
+
+n1=var_int%10
+var_int//=10
+
+n2=var_int%10
+var_int//=10
+
+n3=var_int%10
+n4=var_int//10
+var_int=(n1%2)*n1+(n2%2)*n2+(n3%2)*n3+(n4%2)*n4
+
+print(var_int)
