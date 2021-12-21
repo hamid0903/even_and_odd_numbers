7b61152baf94d160c1e6e39d865a41231af95edc
Hamid Kamalov <hamid_kamalov@mail.ru>
2021-12-21 21:59:21 +0500

    problem4

--- number_odd.py
+++ number_odd.py
@@ -3,3 +3,16 @@
 #Create a variable "var_int" and assign it a four-digit integer value.
 
 #Print the number of odd digits in the variable "var_int".
+var_int=9371
+
+n1=var_int%10
+var_int//=10
+
+n2=var_int%10
+var_int//=10
+
+n3=var_int%10
+n4=var_int//10
+var_int=n1%2+n2%2+n3%2+n4%2
+
+print(var_int)
