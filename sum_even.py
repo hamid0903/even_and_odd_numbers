137a7ee96754309ef37f63a65b2084e9f9e68af2
Hamid Kamalov <hamid_kamalov@mail.ru>
2021-12-21 21:59:48 +0500

    problem5

--- sum_even.py
+++ sum_even.py
@@ -5,3 +5,16 @@
 #Create a variable "sum_even" and assign it 0.
 
 #Find the sum of the even digits in the variable "var_int".
+var_int=8234
+
+n1=var_int%10
+var_int//=10
+
+n2=var_int%10
+var_int//=10
+
+n3=var_int%10
+n4=var_int//10
+var_int=(1-n1%2)*n1+(1-n2%2)*n2+(1-n3%2)*n3+(1-n4%2)*n4
+
+print(var_int)
