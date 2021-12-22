6e9531e44a71da98e4cd0125955e0ba1cba17b42
Hamid Kamalov <hamid_kamalov@mail.ru>
2021-12-22 07:46:52 +0500

    masala-2

--- is_odd.py
+++ is_odd.py
@@ -1,5 +1,5 @@
 #Create a variable "number" and assign it a integer value.
 
-#Print 1 if the number is odd, otherwise print 0.
-number=24
+#Print 1 if the number is odd, otherwise print 0
+number=42 
 print(number%2)
